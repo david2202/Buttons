da0e15da0ff28a05c3abd8921bf1564a5323b41d
David Howe <davidrh2202@gmail.com>
2015-02-16 21:07:40 +1100

    Initial checkin

---
+++ Buttons.cpp
@@ -0,0 +1,113 @@
+#include "Arduino.h"
+#include "Buttons.h"
+
+Button::~Button() {
+  // destructor
+}
+
+DigitalButton::DigitalButton(int pin, byte pMode, int debounceDelay, int repeatInitialDelay, int repeatDelay, buttonCallbackType clickCallback) {
+  _pin = pin;
+  _debounceDelay = debounceDelay;
+  _repeatInitialDelay = repeatInitialDelay;
+  _repeatDelay = repeatDelay;
+  _pinMode = pMode;
+  pinMode(pin, _pinMode);
+  _currentState = 0;
+  _previousState = 0;
+  _lastDebounceTime = 0;
+  _clickCallback = clickCallback;
+}
+
+DigitalButton::DigitalButton(int pin, byte pMode, int debounceDelay, buttonCallbackType clickCallback) {
+  _pin = pin;
+  _debounceDelay = debounceDelay;
+  _repeatInitialDelay = -1;
+  _repeatDelay = -1;
+  _pinMode = pMode;
+  pinMode(pin, _pinMode);
+  _currentState = 0;
+  _previousState = 0;
+  _lastDebounceTime = 0;
+  _clickCallback = clickCallback;
+}
+
+DigitalButton::DigitalButton(int pin, byte pMode, int debounceDelay, int repeatInitialDelay, int repeatDelay, buttonCallbackType clickCallback, buttonCallbackType releaseCallback) {
+  _pin = pin;
+  _debounceDelay = debounceDelay;
+  _repeatInitialDelay = repeatInitialDelay;
+  _repeatDelay = repeatDelay;
+  _pinMode = pMode;
+  pinMode(pin, _pinMode);
+  _currentState = 0;
+  _previousState = 0;
+  _lastDebounceTime = 0;
+  _clickCallback = clickCallback;
+  _releaseCallback = releaseCallback;
+}
+
+DigitalButton::DigitalButton(int pin, byte pMode, int debounceDelay, buttonCallbackType clickCallback, buttonCallbackType releaseCallback) {
+  _pin = pin;
+  _debounceDelay = debounceDelay;
+  _repeatInitialDelay = -1;
+  _repeatDelay = -1;
+  _pinMode = pMode;
+  pinMode(pin, _pinMode);
+  _currentState = 0;
+  _previousState = 0;
+  _lastDebounceTime = 0;
+  _clickCallback = clickCallback;
+  _releaseCallback = releaseCallback;
+}
+
+DigitalButton::~DigitalButton() {
+}
+
+void DigitalButton::read() {
+  int reading = digitalRead(_pin);
+  if (_pinMode == INPUT_PULLUP) {
+    reading = !reading;                          // Need "!" as LOW = pressed (internal pullup)
+  }
+
+  if (reading != _previousState) {               // If button has changed since last time
+    _lastDebounceTime = millis();                // store the time it changed
+  }
+    
+  if (reading != _currentState                   // If the reading is different
+      && (millis() - _lastDebounceTime)          // and more than the debounce delay has elapsed
+          > _debounceDelay) {                    // since it changed
+    _currentState = reading;                     // Store this reading as the new state
+    if (_currentState == HIGH) {                 // If button is pressed
+      _clickCallback(this);                      // Handle the button press
+    } else {                                     // Button is not pressed
+      if (_releaseCallback != NULL) {            // If there is a release callback defined
+        _releaseCallback(this);                  // call it
+      }
+      _repeating = false;                        // Clear the repeating flag on release
+    }
+  }
+    
+  if (reading == HIGH                            // If the button is pressed
+      && _currentState == HIGH) {                // and it was pressed last time  
+    if ((_repeatInitialDelay >= 0 &&             // and repeating is enabled
+        !_repeating &&                           // and we're not currently repeating
+        millis() - _lastDebounceTime             // and the repeatInitialDelay time has passed
+          > _repeatInitialDelay) ||              // OR
+         (_repeating &&                          // We are repeating
+         millis() - _lastDebounceTime            // and the repeatDelayTime has passed
+           > _repeatDelay)) {
+      _repeating = true;                         // Now we are repeating
+      _clickCallback(this);                      // Handle the button press
+      _lastDebounceTime = millis();              // Move the debounce time along for the next repeat
+    }
+  }
+  _previousState = reading;
+}
+
+boolean DigitalButton::isRepeating() {
+  return _repeating;
+}
+
+boolean DigitalButton::isDown() {
+  return _currentState == HIGH;
+}
+
---
+++ Buttons.h
@@ -0,0 +1,50 @@
+/*
+  Buttons.h - Library for working with buttons
+*/
+#ifndef Buttons_h
+#define Buttons_h
+
+#include "Arduino.h"
+
+/*
+  Interface for a button
+*/
+class Button {
+  public:
+    virtual ~Button();
+    virtual void read()=0;
+    virtual boolean isRepeating()=0;
+    virtual boolean isDown()=0;
+};
+
+typedef void (*buttonCallbackType)(Button*);
+
+/*
+  A digital button.  This type of button is connected to a digital pin in either INPUT or INPUT_PULLUP mode.
+*/
+class DigitalButton : public Button {
+  public:
+    DigitalButton(int pin, byte pinMode, int debounceDelay, int repeatInitialDelay, int repeatDelay, buttonCallbackType clickCallback);
+    DigitalButton(int pin, byte pinMode, int debounceDelay, buttonCallbackType clickCallback);
+    DigitalButton(int pin, byte pinMode, int debounceDelay, int repeatInitialDelay, int repeatDelay, buttonCallbackType clickCallback, buttonCallbackType releaseCallback);
+    DigitalButton(int pin, byte pinMode, int debounceDelay, buttonCallbackType clickCallback, buttonCallbackType releaseCallback);
+    ~DigitalButton();
+    void read();
+    boolean isRepeating();
+    boolean isDown();
+
+  private:
+    int _pin;
+    byte _pinMode;
+    int _currentState;
+    int _previousState;
+    long _lastDebounceTime;
+    int _debounceDelay;
+    int _repeatInitialDelay;
+    int _repeatDelay;
+    boolean _repeating;
+    buttonCallbackType _clickCallback;
+    buttonCallbackType _releaseCallback = NULL;
+};
+
+#endif
